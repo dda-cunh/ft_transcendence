9b101ab5a0e0bd4d6f60eae02d1434be327c6cfb
dda-cunh <dda-cunh@student.42lisboa.com>
2025-02-12 01:56:44 +0000

    can get tokens, refresh, create user and delete user

--- core/auth/auth/urls.py
+++ core/auth/auth/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for api project.
+URL configuration for auth project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
+from .views import RegisterView, DeleteUserView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('delete/', DeleteUserView.as_view(), name='delete'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
 ]
--- core/auth/docker_conf.sh
+++
@@ -1,5 +0,0 @@
-echo "Running database migrations..."
-python manage.py migrate --noinput
-
-echo "Starting Gunicorn server on port $AUTH_PY_SERV_PORT..."
-gunicorn -w 4 -b "0.0.0.0:$AUTH_PY_SERV_PORT" auth.wsgi:application
