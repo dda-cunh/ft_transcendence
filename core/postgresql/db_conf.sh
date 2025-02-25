47907c6a629e774e04fb9fc8988f787c88f81951
dda-cunh <dda-cunh@student.42lisboa.com>
2025-02-25 10:09:31 +0000

    still unstable

--- core/postgresql/db_conf.sh
+++ core/postgresql/db_conf.sh
@@ -3,21 +3,21 @@
 db_is_setup="${HOME}/.is_setup"
 
 if [ ! -f $db_is_setup ]; then
-    pg_createcluster $PG_CLUSTER
+	initdb -D $PGDATA
 
-	pg_ctlcluster $PG_VERSION $PG_CLUSTER start
+    echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
+    echo "host all all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
 
-	psql --username=postgres --command "CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASS';"
-	psql --username=postgres --command "CREATE DATABASE $DB_NAME;"
-	psql --username=postgres --command "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
+    pg_ctl -D $PGDATA start
 
-	pg_ctlcluster $PG_VERSION $PG_CLUSTER stop
+    psql --username=postgres --command "CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASS';"
+    psql --username=postgres --command "CREATE DATABASE $DB_NAME;"
+    psql --username=postgres --command "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
 
-	echo "listen_addresses='*'" >> $PGDATA/postgresql.conf
-	echo "host all all 0.0.0.0/0 md5" >> $PGDATA/pg_hba.conf
+    pg_ctl -D $PGDATA stop
 
 	touch $db_is_setup
 fi
 
-exec   /usr/lib/postgresql/$PG_VERSION/bin/pg_ctl -D $PGDATA start
+postgres -D $PGDATA -p $DB_PORT -h "0.0.0.0"
 
