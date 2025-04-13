e765696ba55adffe48b2e473a9bb97c3d1c18432
dda-cunh <dda-cunh@student.42lisboa.com>
2025-04-13 20:52:26 +0100

    more game logic

--- core/game/ServerPong/game_logic.py
+++ core/game/ServerPong/game_logic.py
@@ -1,4 +1,38 @@
+import random
+
 from ServerPong.game_utils import *
 
-def get_next_frame(state: GameState, x: UpdateVars)-> GameState:
-	return
+def	check_goal(old_ball_pos: Point2D, ball_pos: Point2D,
+				p1_pos: Point2D, p2_pos: Point2D)-> bool:#TODO:
+	return (False)
+
+def	do_player_action(action: KeyState, curr_coords: Point2D)-> Point2D:#TODO:
+	if action == KeyState.UP:
+		return Point2D(curr_coords.x, max(0, curr_coords.y - PADDLE_SPEED))
+	elif action == KeyState.DOWN:
+		return Point2D(curr_coords.x, min(CANVAS_H - PADDLE_HEIGHT, curr_coords.y + PADDLE_SPEED))
+	return curr_coords
+
+def	do_ball_move(ball_pos: Point2D, ball_vec: Vec2D,
+					p1_pos: Point2D, p2_pos: Point2D)-> Point2D:#TODO:
+	#Should check for colisions with walls an
+	new_ball_pos = ball_vec.apply_to_point(ball_pos)
+	return (new_ball_pos)
+
+def	get_next_frame(old: GameState, actions: PlayersActions)-> GameState:
+	new = GameState()
+	new.p1_pos = do_player_action(actions.player_1_key_scale)
+	new.p2_pos = do_player_action(actions.player_2_key_scale)
+	new.ball_vec = old.ball_vec
+	if (new.ball_vec.x == 0 and new.ball_vec.y == 0):
+		angle = random.uniform(0, 2 * math.pi)
+		new.ball_vec = Vec2D(math.cos(angle), math.sin(angle))
+	new.ball_vec.normalize()
+	new.ball_vec.scale(FRAME_MOVE_PX)
+	new.ball_pos = do_ball_move(old.ball_pos, new.ball_vec, new.p1_pos, new.p2_pos)
+	if check_goal(old.ball_pos, new.ball_pos, new.p1_pos, new.p2_pos):
+		if new.ball_pos.x > CANVAS_MID_X:
+			new.p1_score = old.p1_score + 1
+		else:
+			new.p2_score = old.p2_score + 1
+	return (new)
--- core/game/ServerPong/game_utils.py
+++ core/game/ServerPong/game_utils.py
@@ -6,16 +6,35 @@ from enum import Enum
 
 CANVAS_H = 600
 CANVAS_W = 800
-FRAME_MOVE_PX = 0.5
+CANVAS_MID_X = 0
+CANVAS_MID_Y = 0
+FRAME_MOVE_PX = 1.2
 PADDLE_HEIGHT = CANVAS_H / 4.5
 PADDLE_WIDTH = 40
+PADDLE_SPEED = 10
 BALL_SIZE = 125
 SCORE_TO_WIN = 3
 
 @dataclass
 class	Point2D:
+	"""Represents a point in 2D space."""
 	x: float
 	y: float
+
+class	Line:
+	"""Represents a line in 2D space in the general form Ax + By + C = 0."""
+	def	__init__(self, a: float, b: float, c: float):
+		self.a = a
+		self.b = b
+		self.c = c
+
+	def	intersect(self, other: 'Line')-> Point2D | None:
+		determinant = self.a * other.b - other.a * self.b
+		if determinant == 0:
+			return None
+		x = (self.b * other.c - other.b * self.c) / determinant
+		y = (self.c * other.a - other.c * self.a) / determinant
+		return (Point2D(x, y))
 
 class	Vec2D:
 	def	__init__(self, x: float = 0, y: float = 0):
@@ -32,31 +51,48 @@ class	Vec2D:
 			self.y = 0
 		return
 
+	def	scale(self, factor: float):
+		self.x *= factor
+		self.y *= factor
+		return
+
+	def	make_line(self, point: Point2D)-> Line:
+		a = self.y
+		b = -a
+		c = a * point.y - b * point.x
+		return (Line(a, b, c))
+
 	def	apply_to_point(self, point: Point2D) -> Point2D:
 		return (Point2D(point.x + self.x, point.y + self.y))
 
 	@classmethod
 	def	from_points(cls, start: Point2D, end: Point2D) -> 'Vec2D':
-		return cls(end.x - start.x, end.y - start.y)
+		return (cls(end.x - start.x, end.y - start.y))
 
 @dataclass
 class	GameState:
-	player_1_id: uuid
-	player_2_id: uuid
-	player_1_pos: Point2D
-	player_2_pos: Point2D
-	player_1_score: int
-	player_2_score: int
+	# p1_id: uuid
+	# p2_id: uuid
+	p1_pos: Point2D
+	p2_pos: Point2D
+	p1_score: int
+	p2_score: int
 	ball_pos: Point2D
 	ball_vec: Vec2D
- 
+
+dataclass
+class	RedisGameVars:
+	p1_id: uuid
+	p2_id: uuid
+	state: GameState
+
 class	KeyState(Enum):
 	UP = 1
 	IDLE = 0
 	DOWN = -1
 
 @dataclass
-class	UpdateVars:
-	player_1_key_scale: KeyState
-	player_2_key_scale: KeyState
+class	PlayersActions:
+	p1_key_scale: KeyState
+	p2_key_scale: KeyState
  
