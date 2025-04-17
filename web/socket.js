5c0d0520066facb9c43414e1395063b1b4c64ed1
Margarida <aguiar.m.margarida@gmail.com>
2025-04-17 17:55:45 +0100

    corrected typo

--- web/socket.js
+++ web/socket.js
@@ -102,8 +102,8 @@ function drawFrame() {
   ctx.fill();
   ctx.closePath();
 
-  ctx.fillRect(0, gameState.p1_pos_y - gameConstants.paddle_h / 2, gameConstants.paddle_w, gameConstants.paddle_h);
-  ctx.fillRect(gameConstants.canvas_w - gameConstants.paddle_w, gameState.p2_pos_y - gameConstants.paddle_h / 2, gameConstants.paddle_w, gameConstants.paddle_h);
+  ctx.fillRect(0, gameState.p1_pos_y + half_h, gameConstants.paddle_w, gameConstants.paddle_h);
+  ctx.fillRect(gameConstants.canvas_w - gameConstants.paddle_w, gameState.p2_pos_y + half_h, gameConstants.paddle_w, gameConstants.paddle_h);
 
   ctx.font = '20px Arial';
   ctx.fillText(`${gameConstants.p1_name}   ${gameState.p1_score} : ${gameState.p2_score}   ${gameConstants.p2_name}`, canvas.width / 2 - 20, 30);
