b6a9cd2948a6d7566f5eeeb54c9ebf7af8d1a704
dda-cunh <dda-cunh@student.42lisboa.com>
2025-04-18 19:35:36 +0100

    Initial game logging

--- core/game/ServerPong/urls.py
+++ core/game/ServerPong/urls.py
@@ -1,7 +1,9 @@
-from django.urls import re_path
+
+from django.urls import path, include, re_path
 from .views import singleGameView, tournamentView
 
 urlpatterns = [
     re_path(r'^singlegame/?$', singleGameView.as_view(), name='singlegame'),
     re_path(r'^tournament/?$', tournamentView.as_view(), name='tournament'),
+    path('tracker/', include('tracker.urls'))
 ]
--- core/game/docker_exec.sh
+++ core/game/docker_exec.sh
@@ -3,6 +3,8 @@
 pip install --no-cache-dir -r requirements.txt
 
 export DJANGO_SETTINGS_MODULE=ServerPong.settings
+
+python3 manage.py makemigrations tracker
 python3 manage.py migrate
 
 daphne --bind 0.0.0.0 --port 8000 ServerPong.asgi:application
