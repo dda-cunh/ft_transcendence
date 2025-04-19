112dd423ead0a2e435b495455e57fa28a07c8b90
pietro <fmouronh@student.42lisboa.com>
2025-04-19 04:56:34 +0100

    working on friend requests

--- web/friend_requests.js
+++ web/friend_requests.js
@@ -16,9 +16,9 @@ export async function	renderFriendRequests()
 			return ;
 
 		let frTable = document.getElementById("frList");
-		let data = await response.json();
+		let responseData = await response.json();
 
-		if (Object.keys(data).length === 0)
+		if (Object.keys(responseData).length === 0)
 		{
 			frTable.innerHTML = `
 				<tr class="align-items-center justify-content-around">
@@ -26,9 +26,33 @@ export async function	renderFriendRequests()
 				</tr>
 			`;
 		}
+		else
+		{
+			responseData.forEach(entry => {
+				console.log(entry);
+				let row = `
+					<tr class="m-0 p-0 w-100 d-flex flex-row align-items-center justify-content-around">
+						<td class=""><img height="75px" class="rounded-circle" src="${entry.sender_avatar}" alt="${entry.sender_username}'s avatar" /></td>
+						<td class="">${entry.sender_username}</td>
+						<td class="">
+							<button class="btn btn-outline-success">ACCEPT</button>
+							<button class="btn btn-outline-danger">DENY</button>
+						</td>
+					</tr>
+				`;
+				frTable.innerHTML += row;
+			} );
+		}
 	}
 	catch (error)
 	{
 		alert(error);
 	}
 }
+
+/*
+{"id":"f4bc100d-0d42-4bba-8612-2525ef66a325","username":"fmouronh","motto":"git gud","avatar":"avatars/ballspin_ExpHtQF.png","last_activity":"2025-04-19T03:44:56.279307Z"}
+
+{"id":1,"sender":"a8573cc1-132f-461b-9551-145abec1e569","receiver":"f4bc100d-0d42-4bba-8612-2525ef66a325","sent_at":"2025-04-19T03:49:09.959506Z","accepted_at":null,"rejected_at":null}
+
+*/
