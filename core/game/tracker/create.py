971a5f24eeccba3c8b15901cd37bfc8e921c14a6
dda-cunh <dda-cunh@student.42lisboa.com>
2025-04-19 17:53:18 +0100

    tracking fix

--- core/game/tracker/create.py
+++ core/game/tracker/create.py
@@ -7,14 +7,14 @@ from .serializers import (
 def	save_tournament_history(data):
 	serializer = TournamentHistorySerializer(data=data)
 	if serializer.is_valid(raise_exception=True):
-		return serializer.save()
+		return serializer.save().to_dict()
 
 def	save_match_history(data):
 	serializer = MatchHistorySerializer(data=data)
 	if serializer.is_valid(raise_exception=True):
-		return serializer.save()
+		return serializer.save().to_dict()
 
 def	save_tournament_match_history(data):
 	serializer = TournamentMatchHistorySerializer(data=data)
 	if serializer.is_valid(raise_exception=True):
-		return serializer.save()
+		return serializer.save().to_dict()
--- core/game/tracker/serializers.py
+++ core/game/tracker/serializers.py
@@ -15,6 +15,8 @@ class	MatchHistorySerializer(serializers.ModelSerializer):
 	def	validate(self, data):
 		if data['winner'] not in [data['player1'], data['player2']]:
 			raise serializers.ValidationError("Winner must be one of the players.")
+		if data['player1'] == data['player2']:
+			raise serializers.ValidationError("Nice try") #>_< b-baka
 		return data
 
 
