659c73f614475565bcb15db28884822224bedaa0
Margarida <aguiar.m.margarida@gmail.com>
2025-04-20 11:43:44 +0100

    deleted unused endpoints

--- core/game/ServerPong/urls.py
+++ core/game/ServerPong/urls.py
@@ -1,9 +1,6 @@
 
 from django.urls import path, include, re_path
-from .views import singleGameView, tournamentView
 
 urlpatterns = [
-    re_path(r'^singlegame/?$', singleGameView.as_view(), name='singlegame'),
-    re_path(r'^tournament/?$', tournamentView.as_view(), name='tournament'),
     path('tracker/', include('tracker.urls'))
 ]
