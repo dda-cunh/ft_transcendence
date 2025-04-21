2c2cd7028d52f2ff660ada94421ad57d48b81475
pietro <fmouronh@student.42lisboa.com>
2025-04-21 04:47:59 +0100

    fixing design of friends list in profile view

--- web/profile.js
+++ web/profile.js
@@ -22,8 +22,8 @@ export async function renderFriendsList()
 			return
 		let data = await response.json();
 		data.forEach(entry => {
-            let row = `<tr class="m-0 p-0 w-100 d-flex flex-row align-items-center justify-content-around border-top">
-				<td class="d-flex flex-column"><img height="75px" class="rounded-circle" src="/management/media/${entry.avatar}" alt="${entry.username}'s avatar" /></td>
+            let row = `<tr class="m-0 p-0 w-100 d-flex flex-row align-items-center justify-content-around">
+				<td class="d-flex flex-column"><img style="height: 75px; width: 75px; object-fit: cover;" class="rounded-circle" src="/management/media/${entry.avatar}" alt="${entry.username}'s avatar" /></td>
 				<td class="d-flex flex-column">${entry.username}</td>
             </tr>`;
             dest.innerHTML += row;
@@ -37,7 +37,7 @@ export async function renderFriendsList()
 
 
 	/*	MAIN FUNCTION	*/
-export function	renderProfile()	//	ADD id PARAMETER
+export function	renderProfile()
 {
 	renderFriendsList();
 }
