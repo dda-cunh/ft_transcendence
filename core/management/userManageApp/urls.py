706154afe08494a9fd63e7123c107e230e01d01e
dlbltv <idlbltv@gmail.com>
2025-04-21 17:08:28 +0100

    fix: clean and add url for public list friends

--- core/management/userManageApp/urls.py
+++ core/management/userManageApp/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import re_path
-from .views import UpdateLoginView, UpdatePasswordView, UpdateMottoView, UpdateAvatarView, PublicUserDetailView, PrivateUserInfoView, FriendRequestCreateView, FriendRequestAcceptView, FriendRequestRejectView, FriendListView, PendingFriendRequestsView
+from .views import UpdateLoginView, UpdatePasswordView, UpdateMottoView, UpdateAvatarView, PublicUserDetailView, PrivateUserInfoView, FriendRequestCreateView, FriendRequestAcceptView, FriendRequestRejectView, FriendListView, PendingFriendRequestsView, PublicFriendListView
 
 urlpatterns = [
     re_path(r'^username/?$', UpdateLoginView.as_view(), name='update_username'),
@@ -8,6 +8,7 @@ urlpatterns = [
     re_path(r'^motto/?$', UpdateMottoView.as_view(), name='update_motto'),
     re_path(r'^avatar/?$', UpdateAvatarView.as_view(), name='update_avatar'),
     re_path(r'^user/(?P<user_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/?$', PublicUserDetailView.as_view(), name='public_user_detail'),
+    re_path(r'^user/(?P<user_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/friends/?$', PublicFriendListView.as_view(), name='public_friend_list'),
     re_path(r'^user/?$', PrivateUserInfoView.as_view(), name='private_user_info'),
     re_path(r'^friends/request/?$', FriendRequestCreateView.as_view(), name='friend_request_create'),
     re_path(r'^friends/request/(?P<pk>\d+)/accept/?$', FriendRequestAcceptView.as_view(), name='friend_request_accept'),
