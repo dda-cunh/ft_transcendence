6a0429b49d04bc7f8ed4f2256b599cc21952004b
dda-cunh <dda-cunh@student.42lisboa.com>
2025-04-21 23:32:02 +0100

    player score tracking in db

--- core/game/tracker/models.py
+++ core/game/tracker/models.py
@@ -1,12 +1,13 @@
 import json
 from django.db import models
 from django.conf import settings
+from datetime import datetime
 
 
 class	TournamentHistory(models.Model):
 	id = models.BigAutoField(primary_key=True)
 	winner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	ended_at = models.DateTimeField(auto_now_add=True, null=True)
+	ended_at = models.DateTimeField(auto_now_add=True, blank=True)
 
 	def	to_dict(self):
 		return {
@@ -26,17 +27,21 @@ class	MatchHistory(models.Model):
 								related_name='matches_as_player1')
 	player2 = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
 								related_name='matches_as_player2')
+	p1_score = models.IntegerField(default=0, null=False)
+	p2_score = models.IntegerField(default=0, null=False)
 	winner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
 							   related_name='matches_won')
-	ended_at = models.DateTimeField(auto_now_add=True, null=True)
+	ended_at = models.DateTimeField(auto_now_add=True, blank=True)
 
 	def	to_dict(self):
 		return {
 			"id": self.id,
 			"player1": self.player1.id,
 			"player1_username": self.player1.username,
+			"p1_score": self.p1_score,
 			"player2": self.player2.id,
 			"player2_username": self.player2.username,
+			"p2_score": self.p2_score,
 			"winner": self.winner.id,
 			"winner_username": self.winner.username,
 			"ended_at": self.ended_at or None
